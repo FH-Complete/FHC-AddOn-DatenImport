b15707ae44b2b484de1a6cdedb980497b4707722
oesi <oesi@technikum-wien.at>
2015-05-05 17:07:18 +0200

    Fehler behoben bei Timestampvergleich mit NULL Values

--- vilesci/dis_sync.php
+++ vilesci/dis_sync.php
@@ -82,6 +82,9 @@ ini_set('memory_limit', '1024M');
 					if (preg_match('#^(\d{4})\D?(0[1-9]|1[0-2])\D?([12]\d|0[1-9]|3[01])$#',$map->diq_attribute))
 						$where.=' OR '.$map->diq_attribute.'::'.$map->fhc_datatype.'!='.$map->fhc_attribute.'';
 					break;
+				case 'time': // timestamp
+					$where.=' OR COALESCE('.$map->diq_attribute.',\'1970-01-01\')::'.$map->fhc_datatype.'!=COALESCE('.$map->fhc_attribute.',\'1970-01-01\')';
+					break;
 				case  'char':
 				case  'varc':
 				case  'text':
